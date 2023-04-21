dee99e40717e20ddd31bf14d7d15cc0ef44ddf02
liz-lovelace <ivanov0604041@gmail.com>
2023-04-21 15:42:35 +0600

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,86 @@
+<script>
+  import SocialButton from "./SocialButton.svelte";
+</script>
+
+<div class="spacebg1" />
+<div class="spacebg2" />
+<main>
+  <h1>Liz Lovelace</h1>
+  <h2>About me</h2>
+  <p>
+    My two main interests are Programming and Sex. All of my other interests
+    derive from a combination of those two :D
+  </p>
+  <p>
+    In sex/relationships I'm a: transgender (mtf) polyamorous pansexual kinkster
+  </p>
+  <p>I like code that is: pragmatic, elegant, declarative and useful</p>
+  <h2>My social media</h2>
+  <SocialButton
+    website="Twitter (MAIN)"
+    link="https://twitter.com/liz_love_lace"
+    handle="@liz_love_lace"
+    icon="twitter-icon.svg"
+    description="Where I post bangers"
+  />
+  <SocialButton
+    website="Twitter (NSFW)"
+    link="https://twitter.com/lace_love_liz"
+    handle="@lace_love_liz"
+    icon="twitter-icon.svg"
+    description="Where I post nudes"
+  />
+  <SocialButton
+    website="Telegram"
+    link="https://t.me/liz_lovelace"
+    handle="@Liz_Lovelace"
+    icon="/telegram-icon.svg"
+    description="Let chat! DMs open!"
+  />
+  <SocialButton
+    website="Discord"
+    link="https://discordapp.com/users/967498063121903686"
+    handle="LizLovelace#2420"
+    icon="discord-icon.svg"
+    description="Let's chat (for gamers)"
+  />
+  <SocialButton
+    website="GitHub"
+    link="https://github.com/Liz-Lovelace"
+    handle="Liz-Lovelace"
+    icon="github-icon.svg"
+    description="Source code for projects"
+  />
+  <SocialButton
+    website="MyAnimeList"
+    link="https://myanimelist.net/animelist/Liz-Lovelace"
+    handle="Liz-Lovelace"
+    icon="myanimelist-icon.png"
+    description="Anime I've seen"
+  />
+  <div class="center-vertically">
+    <p>I stole the design for this page from</p>
+    <a href="https://heckscaper.com/main.html"
+      ><img alt="Emma Essex" src="emmalink.gif" />
+    </a>
+  </div>
+</main>
+
 <style>
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "alagard", sans-serif;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 4rem;
+  }
+
   .spacebg1,
   .spacebg2 {
     width: 100%;
@@ -11,18 +93,18 @@
   }
 
   .spacebg1 {
-    animation: backgroundScroll1 20s linear infinite;
+    animation: backgroundScroll1 30s linear infinite;
     background-image: url("space_bg1.gif");
     z-index: -1;
   }
 
   .spacebg2 {
-    animation: backgroundScroll2 45s linear infinite;
+    animation: backgroundScroll2 55s linear infinite;
     background-image: url("space_bg2.gif");
     z-index: -2;
   }
 
-    @keyframes backgroundScroll1 {
+  @keyframes backgroundScroll1 {
     0% {
       background-position: 0px 0px;
       filter: opacity(100%);
@@ -51,39 +133,20 @@
       filter: opacity(50%);
     }
   }
-</style>
 
-<div class="spacebg1" />
-<div class="spacebg2" />
-<div>
-  <p> Hello </p>
-  <h1> HIII </h1>
-  <h2> HIII </h2>
-  <h3> HIII </h3>
-  <h4> HIII </h4>
-    <p> Hello </p>
-  <h1> HIII </h1>
-  <h2> HIII </h2>
-  <h3> HIII </h3>
-  <h4> HIII </h4>
-    <p> Hello </p>
-  <h1> HIII </h1>
-  <h2> HIII </h2>
-  <h3> HIII </h3>
-  <h4> HIII </h4>
-    <p> Hello </p>
-  <h1> HIII </h1>
-  <h2> HI(flajf;lajfljalfII </h2>
-  <h3> HI(flajf;lajfljalfII </h3>
-  <h4> HI(flajf;lajfljalfII </h4>
-    <p> Hello </p>
-  <h1> HIII </h1>
-  <h2> HIII </h2>
-  <h3> HIII </h3>
-  <h4> II </h4>
-    <p> Hello </p>
-  <h1> HIII </h1>
-  <h2> HIII </h2>
-  <h3> HIII </h3>
-  <h4> HIII </h4>
-</div>
+  main {
+    max-width: 40rem;
+    margin: auto;
+    padding: 0 1.5em;
+  }
+
+  .center-vertically {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  .center-vertically p {
+    margin-right: 10px;
+  }
+</style>
---
+++ src/routes/SocialButton.svelte
@@ -0,0 +1,70 @@
+<script>
+  export let website;
+  export let link;
+  export let handle;
+  export let icon;
+  export let description;
+</script>
+
+<a class="social-button" href={link}>
+  <img src={icon} alt="{website} icon" />
+  <div class="social-button-text">
+    <h3><span class="website">{website} //</span> {description}</h3>
+    <p class="handle">{handle}</p>
+  </div>
+</a>
+
+<style>
+  .social-button {
+    padding: 12px 0px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+    background-color: rgba(20, 0, 20, 0.5);
+    border: 1px solid purple;
+    text-decoration: none;
+    color: white;
+    transition: 0.6s;
+  }
+
+  .social-button:hover {
+    transition: 0.3s;
+    border: 1px solid cyan;
+    text-decoration: none;
+  }
+
+  .social-button img {
+    width: 40px;
+    height: 40px;
+    margin-left: 15px;
+    margin-right: 17px;
+    filter: brightness(0) invert(1);
+  }
+
+  .social-button-text {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .social-button-text h3 {
+    margin: 0;
+    font-size: 1rem;
+  }
+
+  .website {
+    color: #ff69b4;
+  }
+
+  .handle {
+    color: #00ffff;
+    margin-left: 10px;
+    padding-top: 5px;
+  }
+
+  .social-button-text p {
+    margin: 0;
+    margin-left: 10px;
+    font-size: 22px;
+    font-weight: bold;
+  }
+</style>
